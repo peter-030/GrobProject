7bf049bc9d696a56edaee6d20a18720f59e6fa94
chongjunxian <chongjx-wm19@student.tarc.edu.my>
2022-04-07 23:05:38 +0800

    version testing

--- app/src/main/java/com/example/grobproject/MainActivity.kt
+++ app/src/main/java/com/example/grobproject/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //fuck u 123456
+        //fuck u 1234566
     }
 }
